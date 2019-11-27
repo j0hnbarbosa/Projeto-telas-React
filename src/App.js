7ada7b4500df23518f3038f33cf16af037520aa7
j0hnbarbosa <john.contatob@gmail.com>
2019-11-27 00:19:31 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from '../src/components/login';
+import CadastrarCliente from '../src/components/cadastrarCliente';
+import CadastraUsuario from '../src/components/cadastraUsuario';
+import RecuperarSenha from './components/recuperarSenha';
+import NavBar from '../src/components/navBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      {/* <CadastrarCliente /> */}
+      {/* <Login /> */}
+      <RecuperarSenha />
+      {/* <CadastraUsuario /> */}
+    </>
   );
 }
 
---
+++ src/components/login/index.js
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+
+    margin: '30px',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const Login = () => {
+  const [usuario, setUsuario] = useState(null);
+  // useEffect(() => {
+    const fetchData = () => {
+      if (usuario) {
+      axios.post(`http://127.0.0.1:5000/login`, usuario)
+      .then(res => {
+        console.log(res);
+        console.log('Sua Reposta', res.data);
+        setUsuario(res.data);
+      })
+    }
+  }
+  //   fetchData();
+  // }, []);
+
+  const classes = useStyles();
+  const onchange = (event) => {
+    console.log(event)
+    setUsuario({...usuario, [event.target.name]: event.target.value});
+  }
+
+  console.log(usuario)
+
+  return  (
+  <div>
+  <div className={classes.root}>
+    <Grid container spacing={1}>
+      <Grid item xs={3}>
+      <TextField name="email" label="Email" onChange={(event) => onchange(event)} />
+      </Grid>
+      <Grid item xs={3}>
+        <TextField name="senha" label="Senha" onChange={(event) => onchange(event)} />  
+      </Grid>
+    </Grid>
+
+       <Grid item xs={3} />
+      <Grid item xs={3}>
+        <Button variant="contained" onClick={() => fetchData()}>LOGIN</Button>
+      </Grid>
+  </div>
+  </div>
+)
+}
+
+export default Login;
